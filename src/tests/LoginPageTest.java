c14d9f0cd7a29eb81fbee28025adc95a0cd74d4c
sandeeppatil1384 <sandeepvpatil1984@gmail.com>
2024-05-25 19:59:27 +0530

    added/updated files

--- src/tests/LoginPageTest.java
+++ src/tests/LoginPageTest.java
@@ -5,6 +5,10 @@ public class LoginPageTest {
 	public void test1() {
 		
 		System.out.println("test 1 - LP");
+		String un ="admin";
+		String pwd = "admin";
+		
+		System.out.println("logged in ...");
 	}
 
 }
